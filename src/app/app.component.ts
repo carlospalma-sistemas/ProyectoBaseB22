0a31d5a6975e79df9382249ee16ef33ee02e430d
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-09 19:51:40 -0500

    Incorporada calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProyectoBaseB22';
+  title:string = 'ProyectoBaseB22';
+  suma:number = 0;
+  val3:number = 0;
+  val4:number = 0;
+  multiplicacion:number = 0;
+
+  cambiarTitulo():void {
+    this.title = 'Hola cómo están';
+  }
+
+  sumar(val1:string, val2:string):void {
+    this.suma = Number(val1) + Number(val2);
+  }
+
+  multiplicar():void {
+    this.multiplicacion = this.val3 * this.val4;
+  }
 }
